7a4defc6b11900526936ca7b1625e343683f9091
Daniel Inoa <danielinoa@gmail.com>
2016-10-27 23:59:24 -0400

    Updates project to Swift 3

--- DIImageView/DIImageView.swift
+++ DIImageView/DIImageView.swift
@@ -30,7 +30,7 @@ class DIImageView: UIImageView, UITextFieldDelegate {
         addSubview(caption)
         addGestureRecognizer(tapRecognizer)
         addGestureRecognizer(panRecognizer)
-        userInteractionEnabled = true
+        isUserInteractionEnabled = true
         captionCenterY = bounds.height/2
     }
     
@@ -38,11 +38,11 @@ class DIImageView: UIImageView, UITextFieldDelegate {
     
     private lazy var caption: UITextField = {
         let textField = UITextField()
-        textField.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.5)
-        textField.textAlignment = .Center
-        textField.textColor = .whiteColor()
-        textField.tintColor = .whiteColor()
-        textField.keyboardAppearance = .Dark
+        textField.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        textField.textAlignment = .center
+        textField.textColor = .white
+        textField.tintColor = .white
+        textField.keyboardAppearance = .dark
         textField.delegate = self
         return textField
     }()
@@ -56,43 +56,43 @@ class DIImageView: UIImageView, UITextFieldDelegate {
     override func layoutSubviews() {
         super.layoutSubviews()
         let captionSize = CGSize(width: bounds.size.width, height: 32)
-        caption.bounds = CGRect(origin: CGPointZero, size: captionSize)
+        caption.bounds = CGRect(origin: CGPoint.zero, size: captionSize)
         caption.center = CGPoint(x: center.x, y: captionCenterY)
     }
     
     // MARK: - Gestures
     
-    private lazy var tapRecognizer: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped(_:)))
-    private lazy var panRecognizer: UIPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panned(_:)))
+    fileprivate lazy var tapRecognizer: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped(_:)))
+    fileprivate lazy var panRecognizer: UIPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panned(_:)))
     
-    func tapped(sender: AnyObject) {
-        if caption.isFirstResponder() {
+    @objc private func tapped(_ sender: AnyObject) {
+        if caption.isFirstResponder {
             caption.resignFirstResponder()
-            caption.hidden = caption.text?.isEmpty ?? true
+            caption.isHidden = caption.text?.isEmpty ?? true
         } else {
             caption.becomeFirstResponder()
-            caption.hidden = false
+            caption.isHidden = false
         }
     }
     
-    func panned(sender: AnyObject) {
+    @objc private func panned(_ sender: AnyObject) {
         guard let panRecognizer = sender as? UIPanGestureRecognizer else { return }
-        let location = panRecognizer.locationInView(self)
+        let location = panRecognizer.location(in: self)
         captionCenterY = location.y
     }
     
     // MARK: - Text Field Delegate
     
-    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-        guard let captionFont = textField.font where textField == caption && !string.isEmpty else { return true }
-        let textSize = textField.text?.sizeWithAttributes([NSFontAttributeName: captionFont]) ?? CGSizeZero
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let captionFont = textField.font, textField == caption && !string.isEmpty else { return true }
+        let textSize = textField.text?.size(attributes: [NSFontAttributeName: captionFont]) ?? CGSize.zero
         return (textSize.width + 16 < textField.bounds.size.width)
     }
     
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         guard caption == textField else { return true }
         caption.resignFirstResponder()
-        caption.hidden = caption.text?.isEmpty ?? true
+        caption.isHidden = caption.text?.isEmpty ?? true
         return caption.resignFirstResponder()
     }
     
